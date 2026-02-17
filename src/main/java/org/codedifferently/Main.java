b064a6f9bb6716164e0c7da78d9cb29896d7b685
Bryant Ferguson <btbf569708@gmail.com>
2026-02-17 09:39:38 -0500

    Pushing some updated changes

--- src/main/java/org/codedifferently/Main.java
+++ src/main/java/org/codedifferently/Main.java
@@ -7,22 +7,18 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         Customer c1 = new Customer("Bryant", 333);
-        Item i1 = new Item("Cofeee",10.00, true);
-        Item i2 = new Item("Tea",5.00, true);
-        Item i3 = new Item("Croissant",10.00, true);
-        Item i4 = new Item("Cofeee",10.00, true);
-        Item i5 = new Item("Cofeee",10.00, true);
-        Item i6 = new Item("Cofeee",10.00, true);
+        Item i1 = new Item("Coffee",10.00, true);
+        Item i2 = new Item("Tea",5.50, true);
+        Item i3 = new Item("Croissant",12.00, false);
+        Item i4 = new Item("Pie",17.50, false);
+        Item i5 = new Item("Waffles",10.50, false);
+        Item i6 = new Item("Cookies",8.00, false);
 
 
         //Customer c1 = new Customer("Bryant", 333);
         //Customer c1 = new Customer("Bryant", 333);
 
-     //   Purchase.displayMenu(c1, 1);
-
-
-
-
+        Purchase.displayMenu(c1,i1,i2,i3,i4,i5,i6);
 
 
     }
